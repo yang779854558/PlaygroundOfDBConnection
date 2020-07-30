2597f0de34f049c109d4e8726621475adb258b97
yang779854558 <3140801033@stmail.ujs.edu.cn>
2020-07-30 17:46:32 +0800

    yhh

--- PlaygroundOfMybatis/src/main/java/com/yhh/playground/mybatis/T005.java
+++ PlaygroundOfMybatis/src/main/java/com/yhh/playground/mybatis/T005.java
@@ -1,5 +1,10 @@
 package com.yhh.playground.mybatis;
 
+import com.yhh.playground.mybatis.dao.OrderMapper5;
+import com.yhh.playground.mybatis.dao.RoleMapper5;
+import com.yhh.playground.mybatis.dao.UserMapper5;
+import com.yhh.playground.mybatis.entity.Order;
+import com.yhh.playground.mybatis.entity.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -10,6 +15,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 /**
  * 注解
@@ -18,6 +24,9 @@ public class T005 {
 
 
     private SqlSession sqlSession;
+    private OrderMapper5 orderMapper;
+    private UserMapper5 userMapper;
+    private RoleMapper5 roleMapper;
 
     /**
      * 初始化 并 打开连接
@@ -27,6 +36,9 @@ public class T005 {
         InputStream inputStream = Resources.getResourceAsStream("mybatis-config5.xml");
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         sqlSession = sqlSessionFactory.openSession(true);
+        orderMapper = sqlSession.getMapper(OrderMapper5.class);
+        userMapper = sqlSession.getMapper(UserMapper5.class);
+        roleMapper = sqlSession.getMapper(RoleMapper5.class);
     }
 
     /**
@@ -39,8 +51,23 @@ public class T005 {
 
     @Test
     public void test1() {
-
+        List<Order> all = orderMapper.findAll();
+        System.out.println(all);
     }
+
+    @Test
+    public void test2() {
+        List<User> all = userMapper.findAllUserAndOrder();
+        System.out.println(all);
+    }
+
+    @Test
+    public void test3() {
+        List<User> all = userMapper.findAllUserAndRole();
+        System.out.println(all);
+    }
+
+
 
 
 }
--- PlaygroundOfMybatis/src/main/java/com/yhh/playground/mybatis/dao/UserMapper5.java
+++ PlaygroundOfMybatis/src/main/java/com/yhh/playground/mybatis/dao/UserMapper5.java
@@ -1,4 +1,43 @@
 package com.yhh.playground.mybatis.dao;
 
+import com.yhh.playground.mybatis.entity.User;
+import org.apache.ibatis.annotations.Many;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
 public interface UserMapper5 {
+
+    @Select("select * from user where id = #{id}")
+    User findById(int id);
+
+
+    @Select("select * from user")
+    @Results({
+            @Result(id = true,property = "id",column = "id"),
+            @Result(property = "username",column = "username"),
+            @Result(property = "password",column = "password"),
+            @Result(property = "birthday",column = "birthday"),
+            @Result(property = "orderList",column = "id",
+                    javaType = List.class,
+                    many = @Many(select =
+                            "com.yhh.playground.mybatis.dao.OrderMapper5.findByUid"))
+    })
+    List<User> findAllUserAndOrder();
+
+    @Select("select * from user")
+    @Results({
+            @Result(id = true,property = "id",column = "id"),
+            @Result(property = "username",column = "username"),
+            @Result(property = "password",column = "password"),
+            @Result(property = "birthday",column = "birthday"),
+            @Result(property = "roleList",column = "id",
+                    javaType = List.class,
+                    many = @Many(select =
+                            "com.yhh.playground.mybatis.dao.RoleMapper5.findByUid"))
+    })
+    List<User> findAllUserAndRole();
+
 }
